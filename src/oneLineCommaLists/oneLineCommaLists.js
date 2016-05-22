b48b0a81a72b2c7a8d3021f45c20378211e1b0bc
Declan de Wet <declandewet@me.com>
2016-05-23 00:05:28 +0200

    Refactor oneLineCommaLists to use TemplateTag core

--- src/oneLineCommaLists/oneLineCommaLists.js
+++ src/oneLineCommaLists/oneLineCommaLists.js
@@ -1,10 +1,14 @@
 'use strict'
 
-import tags from '../tags'
+import TemplateTag from '../TemplateTag'
+import inlineArrayTransformer from '../inlineArrayTransformer'
+import trimResultTransformer from '../trimResultTransformer'
+import replaceResultTransformer from '../replaceResultTransformer'
 
-export default tags({
-  includeArrays: {
-    separator: ','
-  },
-  oneLine: true
-})
+const oneLineCommaLists = new TemplateTag(
+  inlineArrayTransformer({ separator: ',' }),
+  replaceResultTransformer(/(?:\s+)/g, ' '),
+  trimResultTransformer
+)
+
+export default oneLineCommaLists
