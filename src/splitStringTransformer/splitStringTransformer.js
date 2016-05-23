996029cbab05d9e38d89e9a1f48451c12ac03e5c
Declan de Wet <declandewet@me.com>
2016-05-23 09:34:04 +0200

    add splitStringTransformer

---
+++ src/splitStringTransformer/splitStringTransformer.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const splitStringTransformer = (splitBy) => ({
+  onSubstitution (substitution, resultSoFar) {
+    if (splitBy != null && typeof splitBy === 'string') {
+      if (typeof substitution === 'string' && substitution.includes(splitBy)) {
+        substitution = substitution.split(splitBy)
+      }
+    } else {
+      throw new Error('You need to specify a string character to split by.')
+    }
+    return substitution
+  }
+})
+
+export default splitStringTransformer
