da2d0156a8edf2c97fff131f8ad9e36e189ce5bc
Albert Kurniawan <kurniawan.albert19@gmail.com>
2016-06-21 23:36:29 +1000

    Removed escapeHtml and made replaceSubstitution transformer

---
+++ src/replaceSubstitutionTransformer/replaceSubstitutionTransformer.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const replaceSubstitutionTransformer = (replaceWhat, replaceWith) => ({
+  onSubstitution (substitution, resultSoFar) {
+    if (replaceWhat == null || replaceWith == null) {
+      throw new Error('replaceSubstitutionTransformer requires at least 2 arguments.')
+    }
+
+    // Do not touch if null or undefined
+    if (substitution == null) {
+      return substitution
+    } else {
+      return substitution.toString().replace(replaceWhat, replaceWith)
+    }
+  }
+})
+
+export default replaceSubstitutionTransformer
