653c9277951a129dd1957394dc5045d79257c64c
fatfisz <fatfisz@gmail.com>
2018-09-08 20:19:16 +0200

    Make module test messages a bit clearer

--- src/utils/index.test.js
+++ src/utils/index.test.js
@@ -11,12 +11,15 @@ beforeEach(async () => {
   context.modules = mm(await node.call(fs.readdir, __dirname), observe);
 });
 
+function requireModule(module) {
+  return require(path.join(__dirname, module));
+}
+
 test('utils exports all the right modules directly', async () => {
   const modules = context.modules;
   expect.assertions(modules.length);
   modules.forEach(module => {
-    const _path = path.join(__dirname, module);
-    expect(require(_path) != null).toBe(true);
+    expect(requireModule(module)).toBeDefined();
   });
 });
 
@@ -24,6 +27,6 @@ test('utils exports all the right modules as props', async () => {
   const modules = context.modules;
   expect.assertions(modules.length);
   modules.forEach(module => {
-    expect(require('./index')[module] != null).toBe(true);
+    expect(require('./index')).toHaveProperty(module, requireModule(module));
   });
 });
