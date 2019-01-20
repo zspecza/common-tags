6952ba68198a6aacdb3dfa4a37a9451a2fa3249f
fatfisz <fatfisz@gmail.com>
2019-01-20 19:17:10 +0100

    Improve comments and names

--- src/inlineArrayTransformer/inlineArrayTransformer.js
+++ src/inlineArrayTransformer/inlineArrayTransformer.js
@@ -1,10 +1,10 @@
 /**
  * Converts an array substitution to a string containing a list
- * @param  {String} [opts.separator = ''] - the character that separates each item
- * @param  {String} [opts.conjunction = '']  - replace the last separator with this
- * @param  {Boolean} [opts.serial = false] - include the separator before the conjunction? (Oxford comma use-case)
+ * @param {String} [opts.separator = '']   - The character that separates each item
+ * @param {String} [opts.conjunction = ''] - Replace the last separator with this
+ * @param {Boolean} [opts.serial = false]  - Include the separator before the conjunction? (Oxford comma use-case)
  *
- * @return {Object}                     - a TemplateTag transformer
+ * @return {Object}                        - A transformer
  */
 const inlineArrayTransformer = ({
   conjunction = '',
@@ -17,20 +17,20 @@ const inlineArrayTransformer = ({
       return substitution;
     }
 
-    // be sure to maintain indentation
-    const indent = resultSoFar.match(/(\n?[^\S\n]+)$/);
-    const fullSeparator = separator.concat(indent ? indent[1] : ' ');
+    const indentation = resultSoFar.match(/(\n?[^\S\n]+)$/);
+    const fullSeparator = separator.concat(indentation ? indentation[1] : ' ');
     const fullConjunction = ''.concat(conjunction, ' ');
     const conjunctionIndex = conjunction ? substitution.length - 1 : -1;
 
-    return substitution.reduce((result, part, index) =>
-      ''.concat(
+    return substitution.reduce((result, part, index) => {
+      const shouldUseConjunction = index === conjunctionIndex;
+      return ''.concat(
         result,
-        index !== conjunctionIndex || serial ? fullSeparator : ' ',
-        index === conjunctionIndex ? fullConjunction : '',
+        !shouldUseConjunction || serial ? fullSeparator : ' ',
+        shouldUseConjunction ? fullConjunction : '',
         part,
-      ),
-    );
+      );
+    });
   },
 });
 
